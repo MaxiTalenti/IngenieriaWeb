e70e805ecf4eabf7b766454ed3464200f6a49a4f
Emanuel Colombo <ema_colombo@hotmail.com>
2017-04-19 14:50:37 -0300

    Intentando arreglar eso

---
+++ GlobalEvents/GlobalEvents/App_Start/HtmlHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GlobalEvents.Helpers
+{
+    public static class HtmlHelpers
+    {
+        /// <summary>
+        /// Seleccionando el controlador y/o vista te devuelve una clase o ninguna (Para activar cosas)
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="controllers"></param>
+        /// <param name="actions"></param>
+        /// <param name="cssClass"></param>
+        /// <returns></returns>
+        public static string IsSelected(this HtmlHelper html, string cssClass, string controllers = "", string actions = "")
+        {
+            ViewContext viewContext = html.ViewContext;
+            bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+            if (isChildAction)
+                viewContext = html.ViewContext.ParentActionViewContext;
+
+            RouteValueDictionary routeValues = viewContext.RouteData.Values;
+            string currentAction = routeValues["action"].ToString();
+            string currentController = routeValues["controller"].ToString();
+
+            if (String.IsNullOrEmpty(actions))
+                actions = currentAction;
+
+            if (String.IsNullOrEmpty(controllers))
+                controllers = currentController;
+
+            string[] acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+            string[] acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : String.Empty;
+        }
+
+        public static bool IsSelected(this HtmlHelper html, string controllers = "", string actions = "")
+        {
+            ViewContext viewContext = html.ViewContext;
+            bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+            if (isChildAction)
+                viewContext = html.ViewContext.ParentActionViewContext;
+
+            RouteValueDictionary routeValues = viewContext.RouteData.Values;
+            string currentAction = routeValues["action"].ToString();
+            string currentController = routeValues["controller"].ToString();
+
+            if (String.IsNullOrEmpty(actions))
+                actions = currentAction;
+
+            if (String.IsNullOrEmpty(controllers))
+                controllers = currentController;
+
+            string[] acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+            string[] acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController);
+        }
+    }
+}
