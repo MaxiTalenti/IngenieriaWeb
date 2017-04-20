4c5bd865673859961de0c1c7bfc7cf63b690dfc8
Maximiliano Talenti <maximiliano.talenti@gmail.com>
2017-04-20 03:07:44 -0300

    Cambios

--- GlobalEvents/GlobalEvents/App_Start/BundleConfig.cs
+++ GlobalEvents/GlobalEvents/App_Start/BundleConfig.cs
@@ -9,7 +9,18 @@ namespace GlobalEvents
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/modernizer-2.8.3.js",
+                        "~/Scripts/tether.js",
+                        "~/Scripts/tether.min.js",
+                        "~/Scripts/CustomAlertValidation.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/respond.min.js",
+                        "~/Scripts/mdbs.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,7 +32,10 @@ namespace GlobalEvents
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/respond.min.js",
+                      "~/Scripts/mdbs.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
