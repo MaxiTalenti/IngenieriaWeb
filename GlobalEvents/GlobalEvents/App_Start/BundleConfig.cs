3c82ad752db8e0fe7b98fdc994c37982fb1d152d
Maximiliano Talenti <maximiliano.talenti@gmail.com>
2017-04-22 20:51:58 -0300

    Re-diseño, cambio de framework de frontend, etc. Versiòn preliminar.

--- GlobalEvents/GlobalEvents/App_Start/BundleConfig.cs
+++ GlobalEvents/GlobalEvents/App_Start/BundleConfig.cs
@@ -10,18 +10,13 @@ namespace GlobalEvents
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/modernizer-2.8.3.js",
-                        "~/Scripts/tether.js",
-                        "~/Scripts/tether.min.js",
                         "~/Scripts/CustomAlertValidation.js",
                         "~/Scripts/jquery.validate.js",
                         "~/Scripts/jquery.validate.min.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/bootstrap.min.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/respond.min.js",
-                        "~/Scripts/mdbs.js",
-                        "~/Scripts/mdbs.min.js"));
+                        "~/Scripts/materialize.js",
+                        "~/Scripts/materialize.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -31,21 +26,11 @@ namespace GlobalEvents
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/respond.min.js",
-                      "~/Scripts/mdbs.js",
-                      "~/Scripts/mdbs.min.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
                       "~/Content/font-awesome.min.css",
-                      "~/Content/mdb.css",
-                      "~/Content/mdb.min.css",
-                      "~/Content/style.css"));
+                      "~/Content/materialize.css",
+                      "~/Content/materialize.min.css"));
         }
     }
 }
